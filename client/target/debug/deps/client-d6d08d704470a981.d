0cfbc51f59820a65e0beed37eba2edf7ab56da41
SenukDev <senuk.jay@gmail.com>
2025-07-15 18:44:23 +1000

    Created the client and server projects

---
+++ client/target/debug/deps/client-d6d08d704470a981.d
@@ -0,0 +1,5 @@
+C:\Users\senuk\Desktop\Multiplayer-Networking\client\target\debug\deps\client-d6d08d704470a981.d: src\lib.rs
+
+C:\Users\senuk\Desktop\Multiplayer-Networking\client\target\debug\deps\libclient-d6d08d704470a981.rmeta: src\lib.rs
+
+src\lib.rs:
