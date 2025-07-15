9a68abe17a86554cafe2c936dd0c5e4410cf90e5
SenukDev <senuk.jay@gmail.com>
2025-07-15 18:41:42 +1000

    Revert "Created the initial client and server projects"
    This reverts commit d6d3265441f6109f00e4dfb447f81d8030dce0a9.

--- client/target/debug/deps/client-201299dc25484e95.d
+++
@@ -1,5 +0,0 @@
-C:\Users\senuk\Desktop\Multiplayer-Networking\client\target\debug\deps\client-201299dc25484e95.d: src\main.rs
-
-C:\Users\senuk\Desktop\Multiplayer-Networking\client\target\debug\deps\libclient-201299dc25484e95.rmeta: src\main.rs
-
-src\main.rs:
--- server/target/debug/deps/server-6b92b48878d2a069.d
+++
@@ -1,5 +0,0 @@
-C:\Users\senuk\Desktop\Multiplayer-Networking\server\target\debug\deps\server-6b92b48878d2a069.d: src\main.rs
-
-C:\Users\senuk\Desktop\Multiplayer-Networking\server\target\debug\deps\libserver-6b92b48878d2a069.rmeta: src\main.rs
-
-src\main.rs:
